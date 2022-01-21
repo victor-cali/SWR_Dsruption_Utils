f0fddaa52039cedc4d9f71ae4ca6adcb965230c9
Adrián Alemán-Z <jraleman10@gmail.com>
2022-01-21 15:09:29 +0100

    Update remove_artifacts.m

--- remove_artifacts.m
+++ remove_artifacts.m
@@ -1,4 +1,11 @@
-function [HPC,PFC]=remove_artifacts(HPC,PFC,fn,tr)
+function [HPC,PFC,HPC_filtered,PFC_filtered]=remove_artifacts(HPC,PFC,fn,tr)
+
+%Bandpass the signals before removing artifacts.
+Wn1=[100/(fn/2) 300/(fn/2)]; % Cutoff=100-300 Hz
+[b1,a1] = butter(3,Wn1,'bandpass'); 
+HPC_filtered=filtfilt(b1,a1,HPC);
+PFC_filtered=filtfilt(b1,a1,PFC);
+
 TOT = abs(HPC) + abs(PFC); % Sum of amplitudes ==> To visually assess artifacts, as they will appear in every channel and add up
 
 L = length(HPC(:,1));
@@ -12,9 +19,11 @@ outliers = false(L,1);
 index = 1;
 while index<L
     if TOT(index)>=tr
-        if 300>=index; low_lim = 1; else; low_lim = index-300; end
-        if 1999>length(outliers); high_lim = length(outliers); else; high_lim = index+1999; end
-        outliers(low_lim:high_lim) = ones(length(low_lim:high_lim),1); %When we detect an artifact, remove 300 datapoints prior (build up of artifact) and 2 sec of datapoints after. 
+        if (index-300)>=0
+        outliers((index-300):index+1999) = ones(2300,1); %When we detect an artifact, remove 300 datapoints prior (build up of artifact) and 2 sec of datapoints after.
+        else
+        outliers(1:index+1999) = ones(length(1:index+1999),1); %When we detect an artifact, remove 300 datapoints prior (build up of artifact) and 2 sec of datapoints after.
+        end
         index = index+2000;
     else
         index = index +1;
@@ -26,4 +35,7 @@ end
 
 HPC(outliers,:) = mean(median(HPC)); 
 PFC(outliers,:) = mean(median(PFC));
+
+HPC_filtered(outliers,:) = mean(median(HPC_filtered));
+PFC_filtered(outliers,:) = mean(median(PFC_filtered));
 end
